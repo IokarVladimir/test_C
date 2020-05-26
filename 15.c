3ac7189c60217cd3820aea4d04309f83ceb92c0e
Vladimir <iokar911@gmail.com>
2020-05-27 01:11:12 +0300

    100 примеров на языке Си.Часть 2. Пример 15

---
+++ 15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	double number;
+	printf("Введите число: ");
+	scanf("%lf", &number);
+
+	if (number <= 0.0){
+		if (number == 0.0){
+			printf("Вы ввели 0.\n");
+		} else {
+			printf("Отрицательное.\n");	
+		}
+
+	} else {
+		printf("Положительное.\n");
+	}
+
+	return 0;
+}
