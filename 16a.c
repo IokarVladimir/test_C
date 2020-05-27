00ec1c5c986a45683951c9296d8b6f15d758253a
Vladimir <iokar911@gmail.com>
2020-05-27 23:30:43 +0300

    100 примеров на языке Си.Часть 2. Пример 16

---
+++ 16a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, i, sum = 0;
+
+	printf("Введите положительное целое число: ");
+	scanf("%d", &n);
+
+	for(i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
+	printf("Сумма = %d\n", sum);
+
+	return 0;
+}
---
+++ 16b.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, i, sum = 0;
+
+	printf("Введите  n: ");
+	scanf("%d", &n);
+
+	i = 1;
+	while (i <= n)
+	{
+		sum += i;
+		++i;
+	}
+
+	printf("Сумма = %d\n", sum);
+
+	return 0;
+}
