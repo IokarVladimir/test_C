bd70613af205fa142fbec3b9e75fac568876adcf
Vladimir <iokar911@gmail.com>
2020-05-31 00:09:03 +0300

    100 примеров на языке Си.Часть 2. Пример 23

---
+++ 23.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	long long n;
+	int count = 0;
+
+	printf("Введите целое число: ");
+	scanf ("%lld", &n);
+
+	while (n != 0)
+	{
+		// n = n/10
+		n /= 10;
+		++count;
+	}
+
+	printf ("Количество цифр:  %d\n", count);
+
+	return 0;
+}
