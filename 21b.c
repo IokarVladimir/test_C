3e70d9cdf6f467fb0152f010683938ae5805ee51
Vladimir <iokar911@gmail.com>
2020-05-30 23:30:54 +0300

    100 примеров на языке Си.Часть 2. Пример 21

---
+++ 21b.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int n1, n2;
+
+	printf("Введите два целых числа: ");
+	scanf("%d %d", &n1, &n2);
+
+	while (n1 != n2)
+	{
+		if (n1 > n2)
+			n1 -= n2;
+		else
+			n2 -= n1;
+
+	}
+	printf ("НОД = %d\n", n1);
+
+	return 0;
+}
