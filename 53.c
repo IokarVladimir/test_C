a98ffb194aa6b298f28fe7973b62c11085326582
Vladimir <iokar911@gmail.com>
2020-06-05 07:49:49 +0300

    100 примеров на языке Си.Часть 4. Пример 53

---
+++ 53.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[1000], ch;
+	int i, frequency = 0;
+
+	printf("Введите строку: ");
+	gets (str);
+
+	printf("Введите символ: ");
+	scanf("%c", &ch);
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (ch == str[i])
+			++frequency;
+	}
+
+	printf("Частота = %d\n", frequency);
+
+	return 0;
+}
